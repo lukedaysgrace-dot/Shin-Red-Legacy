a4c1b0c31204d109e52c8d9b8555fb333b18232f
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-02-21 12:53:07 -0600

    Update hitmonchan.asm

--- data/baseStats/hitmonchan.asm
+++ data/baseStats/hitmonchan.asm
@@ -1,9 +1,9 @@
 db DEX_HITMONCHAN ; pokedex id
-db 50 ; base hp
+db 60 ; base hp
 db 105 ; base attack
 db 79 ; base defense
 db 76 ; base speed
-db 75 ; base special
+db 88 ; base special
 db FIGHTING ; species type 1
 db FIGHTING ; species type 2
 db 45 ; catch rate
