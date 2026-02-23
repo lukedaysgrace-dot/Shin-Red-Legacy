68578da47edfb63397aebf02737e4dbfe107514e
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-02-23 11:00:25 -0600

    Update type_constants.asm

--- constants/type_constants.asm
+++ constants/type_constants.asm
@@ -8,7 +8,7 @@ GROUND   EQU $04
 ROCK     EQU $05
 BIRD	 EQU $06	;joenote - added-in this unused type
 BUG      EQU $07
-GHOST    EQU $08
+DRAGON   EQU $09
 ;special types
 FIRE     EQU $14
 WATER    EQU $15
@@ -16,4 +16,4 @@ GRASS    EQU $16
 ELECTRIC EQU $17
 PSYCHIC  EQU $18
 ICE      EQU $19
-DRAGON   EQU $1A
+GHOST    EQU $1B
