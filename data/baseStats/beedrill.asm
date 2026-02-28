382faad61ecb41e68e87aafa210efd2200f475b7
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-02-27 23:59:21 -0600

    bugs

--- data/baseStats/beedrill.asm
+++ data/baseStats/beedrill.asm
@@ -1,9 +1,9 @@
 db DEX_BEEDRILL ; pokedex id
-db 65 ; base hp
-db 80 ; base attack
-db 40 ; base defense
-db 75 ; base speed
-db 45 ; base special
+db 75 ; base hp
+db 100 ; base attack
+db 60 ; base defense
+db 85 ; base speed
+db 65 ; base special
 db BUG ; species type 1
 db POISON ; species type 2
 db 45 ; catch rate
